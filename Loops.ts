efe3dc3dc7071b88e8730d3dc49785fe6a4f48ba
Deepika Raghuraman <deeputharun@Deepikas-MacBook-Air.local>
2026-05-04 10:32:09 -0700

    Your message

---
+++ Loops.ts
@@ -0,0 +1,37 @@
+//Loops
+
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+
+console.log("************");
+
+for(let i=1;i<=50;i++){
+console.log(i)
+}
+console.log("****************");
+//for loop
+//Syntax(rule)
+//let n //declaration
+//let m =20;// intialization
+/*
+for(initalization; condition; increment/decrement)
+{
+    //Block of code to be executed
+}
+*/
+for (let i=0;i<=10;i++){
+console.log(i)
+}
+
+for(let a=10; a>=1;a--){
+    console.log(a)
+}
+
+//while loop
+
+
+
+//do while loop
